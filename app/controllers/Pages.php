a5db7da2ee8c0dda01fca0b6d98506839b221bdb
vikiyev <60604762+vikiyev@users.noreply.github.com>
2023-01-04 03:42:13 +0800

    header and footer

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -11,6 +11,7 @@ class Pages extends Controller {
   }
 
   public function about (){
-    $this->view('pages/about');
+    $data = ['title' => 'About Us'];
+    $this->view('pages/about', $data);
   }
 }
--- app/views/pages/about.php
+++ app/views/pages/about.php
@@ -1,1 +1,3 @@
-ABOUT PAGE
+<?php require APPROOT . '/views/inc/header.php'; ?>
+<h1><?php echo $data['title']; ?></h1>
+<?php require APPROOT . '/views/inc/footer.php'; ?>
