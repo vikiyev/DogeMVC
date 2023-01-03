54bb26726c36bf3af6372fa002fed85c35be1c78
vikiyev <60604762+vikiyev@users.noreply.github.com>
2023-01-04 03:24:36 +0800

    config file and autoloader

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,5 +1,8 @@
 <?php
-// load librarires
-require_once 'libraries/Core.php';
-require_once 'libraries/Controller.php';
-require_once 'libraries/Database.php';
+// load config
+require_once 'config/config.php';
+
+// autoload core libraries
+spl_autoload_register(function($className) {
+  require_once 'libraries/' . $className . '.php';
+});
---
+++ app/config/config.php
@@ -0,0 +1,7 @@
+<?php
+  // App root
+  define('APPROOT', dirname(dirname(__FILE__)));
+  // URL root
+  define('URLROOT', 'http://localhost/dogemvc');
+  // site name
+  define('SITENAME', 'DogeMVC');
